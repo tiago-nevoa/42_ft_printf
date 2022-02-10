3e55305c38e01e6303da500cfcb6c577c98aedbe
Tiago manuel Ferreira nevoa <tferreir@c1r20s8.42lisboa.com>
2022-02-10 19:19:07 +0000

    v0.4

--- ft_printf_Code/ft_printf.c
+++ ft_printf_Code/ft_printf.c
@@ -6,32 +6,11 @@
 /*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 17:33:08 by tferreir          #+#    #+#             */
-/*   Updated: 2022/02/09 20:26:07 by tferreir         ###   ########.fr       */
+/*   Updated: 2022/02/10 17:45:12 by tferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	index;
-
-	index = 0;
-	while (s[index])
-		index++;
-	return (index);
-}
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	if (s)
-		write(fd, s, ft_strlen(s));
-}
+#include "ft_printf.h"
 
 int	ft_printf(const char *format, ...)
 {
@@ -45,21 +24,21 @@ int	ft_printf(const char *format, ...)
 			++i;
 			if (format[i] == 'c')
 				ft_putstr_fd("!caracter!", 1);
-			if (format[i] == 's')
+			else if (format[i] == 's')
 				ft_putstr_fd("!string!", 1);
-			if (format[i] == 'p')
+			else if (format[i] == 'p')
 				ft_putstr_fd("!pointer!", 1);
-			if (format[i] == 'd')
+			else if (format[i] == 'd')
 				ft_putstr_fd("!decimal!", 1);
-			if (format[i] == 'i')
+			else if (format[i] == 'i')
 				ft_putstr_fd("!integer!", 1);
-			if (format[i] == 'u')
+			else if (format[i] == 'u')
 				ft_putstr_fd("!unsigned_decimal!", 1);
-			if (format[i] == 'x')
+			else if (format[i] == 'x')
 				ft_putstr_fd("!hexadecimal_lower!", 1);
-			if (format[i] == 'X')
+			else if (format[i] == 'X')
 				ft_putstr_fd("!hexadecimal_upper!", 1);
-			if (format[i] == '%')
+			else if (format[i] == '%')
 				ft_putchar_fd('%', 1);
 		}
 		else
@@ -67,7 +46,7 @@ int	ft_printf(const char *format, ...)
 	}
 	return (0);
 }
-
+/*
 int	main(void)
 {
 	const char	format[50] = "ABC 123 %d  %c fdsfds %u ][ :\' -/*";
@@ -75,3 +54,4 @@ int	main(void)
 	ft_printf(format);
 	return (0);
 }
+*/
---
+++ ft_printf_Code/ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 15:39:27 by tferreir          #+#    #+#             */
+/*   Updated: 2022/02/10 17:47:10 by tferreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+
+# define FT_PRINTF_H
+
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "./Libft_Code/libft.h"
+
+#endif
