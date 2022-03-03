03cd6dd91720d4713091483881fabf2585c64b4c
Tiago manuel Ferreira nevoa <tferreir@c1r22s1.42lisboa.com>
2022-03-03 20:34:35 +0000

    v0.5

--- ft_printf_Code/ft_printf.c
+++ ft_printf_Code/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 17:33:08 by tferreir          #+#    #+#             */
-/*   Updated: 2022/02/10 17:45:12 by tferreir         ###   ########.fr       */
+/*   Updated: 2022/03/03 20:33:34 by tferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int	ft_printf(const char *format, ...)
 {
-	int	i;
+	int		i;
+	va_list	arguments;
+	va_start (arguments, format);
 
 	i = -1;
 	while (format[++i])
@@ -23,35 +25,40 @@ int	ft_printf(const char *format, ...)
 		{
 			++i;
 			if (format[i] == 'c')
-				ft_putstr_fd("!caracter!", 1);
+				ft_putchar_fd(va_arg(arguments, char), 1);
 			else if (format[i] == 's')
-				ft_putstr_fd("!string!", 1);
-			else if (format[i] == 'p')
+				ft_putstr_fd(va_arg(arguments, char *), 1);
+			else if (format[i] == 'p') // Pointer location in memo on Hexa Ex: 0x7ffee9d48938
 				ft_putstr_fd("!pointer!", 1);
 			else if (format[i] == 'd')
-				ft_putstr_fd("!decimal!", 1);
-			else if (format[i] == 'i')
-				ft_putstr_fd("!integer!", 1);
-			else if (format[i] == 'u')
-				ft_putstr_fd("!unsigned_decimal!", 1);
-			else if (format[i] == 'x')
+				ft_putnbr_fd(va_arg(arguments, int), 1);
+			else if (format[i] == 'i') // check difference from decimal and integer
+				ft_putnbr_fd(va_arg(arguments, int), 1);
+			else if (format[i] == 'u') // usigned int
+				ft_putnbr_fd(va_arg(arguments, unsigned int), 1);
+			else if (format[i] == 'x') // convert to Hex with lowCase
 				ft_putstr_fd("!hexadecimal_lower!", 1);
-			else if (format[i] == 'X')
+			else if (format[i] == 'X') // convert to Hex with UpperCase
 				ft_putstr_fd("!hexadecimal_upper!", 1);
-			else if (format[i] == '%')
+			else if (format[i] == '%') // print '%'
 				ft_putchar_fd('%', 1);
 		}
 		else
 			ft_putchar_fd(format[i], 1);
 	}
+	va_end (arguments);
 	return (0);
 }
-/*
+
 int	main(void)
 {
-	const char	format[50] = "ABC 123 %d  %c fdsfds %u ][ :\' -/*";
+	int	a;
 
-	ft_printf(format);
+	a = 452;
+	ft_printf("ABC 123 %d  %c fdsfds %u ][ :\' -/*", 7, 'h', 8);
+	printf("\n");
+	printf("ABC 123 %d  %c fdsfds %u ][ :\' -/*", 5, 'h', 6);
+	printf("\n'");
+	printf("%p\n", &a);
 	return (0);
 }
-*/
